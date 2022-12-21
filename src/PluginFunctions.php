4bef1ffcc30813ef58225d66efcde905a2f9a75e
pawel-bozemski <paw.boz@gmail.com>
2022-12-21 12:08:35 +0100

    Add array merge to functions

--- src/PluginFunctions.php
+++ src/PluginFunctions.php
@@ -16,8 +16,8 @@ use SplTempFileObject;
  *
  * @codeCoverageIgnore
  */
-class PluginFunctions {
-
+class PluginFunctions
+{
 
 
 	/**
@@ -25,29 +25,30 @@ class PluginFunctions {
 	 *
 	 * @return array<mixed>
 	 */
-	public function get_products( $offset ): array {
+	public function get_products($offset): array
+	{
 		$products = [];
 		$query    = new \WP_Query(
 			[
 				'posts_per_page' => 250,
-				'offset'         => $offset,
-				'post_type'      => [ 'product', 'product_variation' ],
+				'offset' => $offset,
+				'post_type'      => ['product', 'product_variation'],
 			]
 		);
 
-		while ( $query->have_posts() ) {
+		while ($query->have_posts()) {
 			$query->the_post();
-			$product_id = wc_get_product( get_the_ID() );
-			if ( $product_id ) {
-				if ( $product_id->is_type( 'variation' ) ) {
+			$product_id = wc_get_product(get_the_ID());
+			if ($product_id) {
+				if ($product_id->is_type('variation')) {
 					$id = $product_id->get_parent_id();
 				} else {
 					$id = $product_id->get_id();
 				}
 				$product_cats = [];
-				$cats         = get_the_terms( $id, 'product_cat' );
-				if ( is_array( $cats ) ) {
-					foreach ( $cats as $cat ) {
+				$cats         = get_the_terms($id, 'product_cat');
+				if (is_array($cats)) {
+					foreach ($cats as $cat) {
 						$product_cats[] = $cat->name;
 					}
 				} else {
@@ -56,7 +57,7 @@ class PluginFunctions {
 				$products[] = [
 					$product_id->get_name(),
 					$product_id->get_sku(),
-					is_array( $product_cats ) ? implode( ' ', $product_cats ) : $product_cats,
+					is_array($product_cats) ? implode(' ', $product_cats) : $product_cats,
 					$product_id->get_regular_price(),
 					$product_id->get_sale_price() ? $product_id->get_sale_price() : $product_id->get_regular_price(),
 
@@ -73,29 +74,33 @@ class PluginFunctions {
 	 *
 	 * @return void
 	 */
-	public function create_csv( $offset ) {
+	public function create_csv($offset)
+	{
 		$header[] = [
-			__( 'Product name', 'csv-exporter' ),
-			__( 'SKU', 'csv-exporter' ),
-			__( 'Categories', 'csv-exporter' ),
-			__( 'Regural Price', 'csv-exporter' ),
-			__( 'Sale Price', 'csv-exporter' ),
+			__('Product name', 'csv-exporter'),
+			__('SKU', 'csv-exporter'),
+			__('Categories', 'csv-exporter'),
+			__('Regural Price', 'csv-exporter'),
+			__('Sale Price', 'csv-exporter'),
 		];
-		$products = $this->get_products( $offset );
+		$products = $this->get_products($offset);
+		
+
 		try {
-			$file = Writer::createFromFileObject( new SplTempFileObject() );
-			$file->setOutputBOM( Reader::BOM_UTF8 );
-			$file->setDelimiter( ',' );
-			if ( count( $products ) == 0 ) {
-				$products[] = __( 'No products found', 'csv-exporter' );
-				$file->insertOne( $products );
+			$file = Writer::createFromFileObject(new SplTempFileObject());
+			$file->setOutputBOM(Reader::BOM_UTF8);
+			$file->setDelimiter(',');
+			if (count($products) == 0){
+				$products[] = __('No products found', 'csv-exporter');
+				$file->insertOne($products);
 			} else {
-				$file->insertAll( $products );
-			}
+				$products = array_merge($header, $products);
+				$file->insertAll($products);
+			}			 			
 			ob_end_clean();
-			$file->output( 'produkty.csv' );
+			$file->output('produkty.csv');
 			die;
-		} catch ( CannotInsertRecord $e ) {
+		} catch (CannotInsertRecord $e) {
 			$e->getRecord();
 		}
 	}
